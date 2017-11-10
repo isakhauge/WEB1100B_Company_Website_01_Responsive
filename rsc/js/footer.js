f34765bc1e83d060cff3ed42b2ab54cc63a63991
Isak Hauge <isakhauge@gmail.com>
2017-11-11 00:27:27 +0100

    la til jquery på alle sidene.

--- rsc/js/footer.js
+++ rsc/js/footer.js
@@ -1,60 +1,61 @@
 document.write(
-'<section data-footer="main">' +
-    '<div data-content="footer">' +
+    '<section data-footer="main">' +
+        '<div data-content="footer">' +
 
-        '<div data-col="3">' +
-            '<ikea-logo data-logo="footer" onclick="ikea_logo()"></ikea-logo>' +
-            '<div data-space="10"></div>' +
-            '<p>' +
-            '<b>Inter IKEA Holding S.A</b><br>' +
-            'Rue de Grass 100<br>' +
-            '6700 Arlon-Sterpenich<br>' +
-            'Belgia</b>' +
-            '</p>' +
-            '<div data-space="20"></div>' +
-            '<nav data-nav-footer>' +
-                '<p>Kundeservice</p>' +
-                '<ul>' +
-                    '<li><a href="#">Hjem</a></li>' +
-                    '<li><a href="#">Produkt</a></li>' +
-                    '<li><a href="#">Om oss</a></li>' +
-                    '<li><a href="#">Kontakt</a></li>' +
-                '</ul>' +
-            '</nav>' +
+            '<div data-col="3">' +
+                '<ikea-logo data-logo="footer" onclick="ikea_logo()"></ikea-logo>' +
+                '<div data-space="10"></div>' +
+                '<p>' +
+                '<b>Inter IKEA Holding S.A</b><br>' +
+                'Rue de Grass 100<br>' +
+                '6700 Arlon-Sterpenich<br>' +
+                'Belgia</b>' +
+                '</p>' +
+                '<div data-space="20"></div>' +
+                '<nav data-nav-footer>' +
+                    '<p>Kundeservice</p>' +
+                    '<ul>' +
+                        '<li><a href="#">Hjem</a></li>' +
+                        '<li><a href="#">Produkt</a></li>' +
+                        '<li><a href="#">Om oss</a></li>' +
+                        '<li><a href="#">Kontakt</a></li>' +
+                    '</ul>' +
+                '</nav>' +
+            '</div>' +
+
+            '<div data-col="3">' +
+                '<nav data-nav-footer>' +
+                    '<ul>' +
+                        '<li><a href="#">Hjem</a></li>' +
+                        '<li><a href="#">Produkt</a></li>' +
+                        '<li><a href="#">Om oss</a></li>' +
+                        '<li><a href="#">Kontakt</a></li>' +
+                    '</ul>' +
+                '</nav>' +
+            '</div>' +
+
+            '<div data-col="3">' +
+                '<nav data-nav-footer>' +
+                    '<ul>' +
+                        '<li><a href="#">Hjem</a></li>' +
+                        '<li><a href="#">Produkt</a></li>' +
+                        '<li><a href="#">Om oss</a></li>' +
+                        '<li><a href="#">Kontakt</a></li>' +
+                    '</ul>' +
+                '</nav>' +
+            '</div>' +
+
         '</div>' +
+    '</section>' +
 
-        '<div data-col="3">' +
-            '<nav data-nav-footer>' +
-                '<ul>' +
-                    '<li><a href="#">Hjem</a></li>' +
-                    '<li><a href="#">Produkt</a></li>' +
-                    '<li><a href="#">Om oss</a></li>' +
-                    '<li><a href="#">Kontakt</a></li>' +
-                '</ul>' +
-            '</nav>' +
+    '<section data-footer="creds">' +
+        '<div data-content="footer">' +
+
+            '<div data-col="1">' +
+                '<p>Made by Silicon Valley Boys</p>' +
+            '</div>' +
+
         '</div>' +
-
-        '<div data-col="3">' +
-            '<nav data-nav-footer>' +
-                '<ul>' +
-                    '<li><a href="#">Hjem</a></li>' +
-                    '<li><a href="#">Produkt</a></li>' +
-                    '<li><a href="#">Om oss</a></li>' +
-                    '<li><a href="#">Kontakt</a></li>' +
-                '</ul>' +
-            '</nav>' +
-        '</div>' +
-
-    '</div>' +
-'</section>' +
-
-'<section data-footer="creds">' +
-    '<div data-content="footer">' +
-
-        '<div data-col="1">' +
-            '<p>Made by Silicon Valley Boys</p>' +
-        '</div>' +
-
-    '</div>' +
-'</section>'
+    '</section>' +
+    '<script src="rsc/js/smooth_scroll.js"></script>'
 );
